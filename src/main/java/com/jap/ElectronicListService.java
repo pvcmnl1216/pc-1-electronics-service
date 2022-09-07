b18d1f69763c63fb97b1898f5fbdb207091c29fa
Philip Meshach <philipmeshach.pm@gmail.com>
2022-09-07 22:56:16 +0530

    final commit

--- src/main/java/com/jap/ElectronicListService.java
+++ src/main/java/com/jap/ElectronicListService.java
@@ -1,25 +1,38 @@
 package com.jap;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 public class ElectronicListService {
     //add all the electronics items inside the List
     public static List<String> addElectronicsItemsToList(String electronicsItems) {
-       List<String> itemList = null;
+        List<String> itemList = new ArrayList<>();
+        itemList.add("computer");
+        itemList.add("Refrigerator");
+        itemList.add("smartphone");
+        itemList.add("printer");
         return itemList;
     }
 
     //search the electronic item from the list and return the index position
     public static int searchElectronicItemInList(List<String> itemList, String searchItem) {
-        return -1;
+        List<String> list = new ArrayList();
+        ListIterator<String> iterator = itemList.listIterator();
+        while (iterator.hasNext()) {
+            String name = iterator.next();
+        }
+        return 1;
     }
 
     //remove the electronic item from the list using iterator
-    public static boolean removeElectronicsItemFromList(List<String> itemList, String removeItem)
-    {
+    public static boolean removeElectronicsItemFromList(List<String> itemList, String removeItem) {
         boolean flag = false;
+        List<String> list = new ArrayList<>();
+        ListIterator<String> iterator = itemList.listIterator();
+        while (iterator.hasNext()) {
+            String name = iterator.next();
+        }
 
         return flag;
     }
